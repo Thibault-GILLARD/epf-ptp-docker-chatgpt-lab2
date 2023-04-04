704b384df440196b074aef4271374f7e5c43174e
Thibault-GILLARD <117235512+Thibault-GILLARD@users.noreply.github.com>
2023-04-04 12:20:27 +0200

    codegpt

--- hello.py
+++ hello.py
@@ -22,4 +22,27 @@ def chatgpt():
     )
     return completion['choices'][0]['message']['content']
 
+@app.route('/codegpt')
+def codegpt():
+    args = request.args
+    message = args.get("message")
+
+    # Check if the request is for generating code
+    if 'language' in args and 'content' in args:
+        language = args.get('language')
+        content = args.get('content')
+        code = generate_code_from_content(language, content)
+        return code
+
+    # If not, assume it's a chat message
+    completion = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=message,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    message = completion.choices[0].text
+    return message
 
